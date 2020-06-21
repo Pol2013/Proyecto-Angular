838cc5eaa8050d3a82cf1c39c528968d488706fc
Pol Cristobal Paredes <ingenieropol.2013@gmail.com>
2020-06-21 13:42:52 -0500

    agregue componente nuevo en el modulo app y la libreria de bootstrap en json

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-principal',
-  templateUrl: './principal/principal.component.html',
-  styleUrls: ['./principal/principal.component.css']
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent implements OnInit {
 
@@ -22,9 +22,9 @@ export class PrincipalComponent implements OnInit {
   msg:string = '';
 
     employees = [
-    {'name': 'Fazt', position: 'manager', email:'email@email.com'},
-    {'name': 'Juan', position: 'Designer', email:'email@email.com'},
-    {'name': 'Pedro', position: 'Programmer', email:'email@email.com'}
+    {'nombre': 'Fazt', cargo: 'manager', email:'email@email.com'},
+    {'nombre': 'Juan', cargo: 'Designer', email:'email@email.com'},
+    {'nombre': 'Pedro', cargo: 'Programmer', email:'email@email.com'}
   ];
 
   model:any = {};
@@ -48,8 +48,8 @@ export class PrincipalComponent implements OnInit {
   myValue;
   editEmployee(i):void {
     this.hideUpdate = false;
-    this.model2.name = this.employees[i].name;
-    this.model2.position = this.employees[i].position;
+    this.model2.nombre = this.employees[i].nombre;
+    this.model2.cargo = this.employees[i].cargo;
     this.model2.email = this.employees[i].email;
     this.myValue = i;
   }
